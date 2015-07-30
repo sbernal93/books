67a96eb991cff67883f5d353484f2974855b5985
Piero Recchia <piero.recchia@gmail.com>
2015-07-30 12:49:00 -0430

    Added BookNotFoundException class

--- src/AppBundle/Exception/BookNotFoundException.php
+++ src/AppBundle/Exception/BookNotFoundException.php
@@ -1,13 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: recchia
- * Date: 30/07/15
- * Time: 12:44 PM
- */
 
 namespace AppBundle\Exception;
 
-class BookNotFoundException
+class BookNotFoundException extends \Exception
 {
 }
