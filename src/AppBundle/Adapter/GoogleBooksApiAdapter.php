fc7d5d77bdc1a0e0518d55d882c496c6d09e1937
Santiago Bernal <santiagobernal93@gmail.com>
2015-08-04 13:02:42 -0430

    Added the Google Books Api Adapter

---
+++ src/AppBundle/Adapter/GoogleBooksApiAdapter.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace AppBundle\Adapter;
+
+use AppBundle\Interfaces\AdapterInterface;
+use AppBundle\Exception\ApiException;
+use AppBundle\Entity\Book;
+use Google_Client;
+use Google_Service_Books;
+use Google_Service_Exception;
+use AppBundle\Exception\BookNotFoundException;
+use AppBundle\Model\Constants;
+use Symfony\Component\Config\Definition\Exception\Exception;
+
+class GoogleBooksApiAdapter implements AdapterInterface
+{
+    /**
+     * @var Google_Client
+     */
+    protected $client;
+
+    /**
+     * @var Google_Service_Books
+     */
+    protected $booksApi;
+
+    /**
+     * @var array
+     */
+    protected $params = [];
+
+    public function __construct(array $config)
+    {
+        $this->client = new Google_Client();
+        $this->client->setApplicationName('');
+        $this->client->setDeveloperKey($config[Constants::GOOGLE_BOOKS_LABEL_API_KEY]);
+        $this->booksApi = new Google_Service_Books($this->client);
+        $this->params[Constants::GOOGLE_BOOKS_LABEL_LANGRES] = Constants::GOOGLE_BOOKS_LANGRESTRICT;
+    }
+
+    public function findOne($isbn)
+    {
+        try {
+            $q = Constants::GOOGLE_BOOKS_QUERY . $isbn;
+            $result = $this->booksApi->volumes->listVolumes($q, $this->params);
+            $items = $result->getItems();
+            if (count($items) > 0) {
+                $volumeInfo = $items[0]->getVolumeInfo();
+
+                return $this->buildBookWithApiInfo($volumeInfo);
+            } else {
+                throw new BookNotFoundException("Google Book Api can't find ISBN: " . $isbn);
+            }
+        } catch (Google_Service_Exception $e) {
+            throw new ApiException($e->getMessage());
+        }
+    }
+
+    public function find(array $isbns)
+    {
+        $data = [];
+        try {
+            if (is_array($isbns)) {
+
+                foreach ($isbns as $isbn) {
+                    $q = Constants::GOOGLE_BOOKS_QUERY . $isbn;
+                    $result = $this->booksApi->volumes->listVolumes($q, $this->params);
+                    $items = $result->getItems();
+                    if (count($items) > 0) {
+                        $volumeInfo = $items[0]->getVolumeInfo();
+                        $book = $this->buildBookWithApiInfo($volumeInfo);
+                        $data[] = $book;
+                    }
+                }
+
+                return $data;
+            }
+        }
+        catch(Exception $e)
+        {
+            //The reason for this is if the api limit is exceeded then this will still return some
+            //of the data found without loosing that information
+            //TODO: catch specific exception, set global variable that will inform the system that the api limit is exceeded
+            return $data;
+        }
+
+    }
+
+    /**
+     * Builds a Book entity from the data retrieved from the API
+     *
+     * @param $volumeInfo
+     *
+     * @return Book
+     */
+    public function buildBookWithApiInfo($volumeInfo)
+    {
+        $numIsbn13 = 0;
+        $numIsbn10 = 1;
+        if(strlen($volumeInfo[Constants::GOOGLE_BOOKS_LABEL_INDUSTRY][0][Constants::GOOGLE_BOOKS_LABEL_IDENTIFIER]) != 13) {
+            $numIsbn10 = 0;
+            $numIsbn13 = 1;
+        }
+
+        if (isset($volumeInfo[Constants::GOOGLE_BOOKS_LABEL_INDUSTRY][$numIsbn13])) {
+            $isbn13 = $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_INDUSTRY][$numIsbn13][Constants::GOOGLE_BOOKS_LABEL_IDENTIFIER];
+        }
+        else
+        {
+            $isbn13 = "N/A";
+        }
+        if (isset($volumeInfo[Constants::GOOGLE_BOOKS_LABEL_INDUSTRY][$numIsbn10])) {
+            $isbn10 = $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_INDUSTRY][$numIsbn10][Constants::GOOGLE_BOOKS_LABEL_IDENTIFIER];
+        }
+        else
+        {
+            $isbn10 = "N/A";
+        }
+
+        $author = (is_array($volumeInfo[Constants::GOOGLE_BOOKS_LABEL_AUTHORS])) ?
+            implode(', ', $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_AUTHORS]) :
+            $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_AUTHORS];
+
+
+        $imageLink = (!empty($volumeInfo[Constants::GOOGLE_BOOKS_LABEL_IMAGELINKS][Constants::GOOGLE_BOOKS_LABEL_THUMBNAIL]))
+            ? $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_IMAGELINKS][Constants::GOOGLE_BOOKS_LABEL_THUMBNAIL] : '';
+
+
+        $book = Book::buildComplete(
+            $isbn10,
+            $isbn13,
+            $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_TITLE],
+            $author,
+            $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_PUBLISHER],
+            $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_DESCRIPTION],
+            $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_PAGECOUNT],
+            $imageLink
+        );
+        return $book;
+    }
+
+}
---
+++ src/AppBundle/Model/Constants.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Santiago
+ * Date: 8/4/2015
+ * Time: 10:42 AM
+ */
+
+namespace AppBundle\Model;
+
+class Constants
+{
+    /**
+     * ################################################################################################################
+     *                                          Google Books API Info
+     * ################################################################################################################
+     */
+    const GOOGLE_BOOKS_QUERY            =   'isbn:';
+    const GOOGLE_BOOKS_LABEL_INDUSTRY   =   'industryIdentifiers';
+    const GOOGLE_BOOKS_LABEL_IDENTIFIER =   'identifier';
+    const GOOGLE_BOOKS_LABEL_AUTHORS    =   'authors';
+    const GOOGLE_BOOKS_LABEL_MODELDATA  =   'modelData';
+    const GOOGLE_BOOKS_LABEL_IMAGELINKS =   'imageLinks';
+    const GOOGLE_BOOKS_LABEL_THUMBNAIL  =   'thumbnail';
+    const GOOGLE_BOOKS_LABEL_TITLE      =   'title';
+    const GOOGLE_BOOKS_LABEL_PUBLISHER  =   'publisher';
+    const GOOGLE_BOOKS_LABEL_DESCRIPTION=   'description';
+    const GOOGLE_BOOKS_LABEL_PAGECOUNT  =   'pageCount';
+    const GOOGLE_BOOKS_LABEL_API_KEY    =   'api_key';
+    const GOOGLE_BOOKS_LABEL_LANGRES    =   'langRestrict';
+    const GOOGLE_BOOKS_LANGRESTRICT     =   'es';
+
+    /**
+     * ################################################################################################################
+     *                                          Excel Info
+     * ################################################################################################################
+     */
+    const EXCEL_CREATOR                 =   'Linio Books';
+    const EXCEL_LASTMODIFIED            =   'Linio Books';
+    const EXCEL_TITLE                   =   'Linio Books Search Results';
+    const EXCEL_SUBJECT                 =   'Books';
+    const EXCEL_DESCRIPTION             =   'Linio Books Search Results Powered by Google';
+    const EXCEL_KEYWORDS                =   'office 2007 books';
+    const EXCEL_CATEGORY                =   'Search Result File';
+    const EXCEL_CELL_A1                 =   'ISBN_10';
+    const EXCEL_CELL_B1                 =   'ISBN_13';
+    const EXCEL_CELL_C1                 =   'Titulo';
+    const EXCEL_CELL_D1                 =   'Autor';
+    const EXCEL_CELL_E1                 =   'Editorial';
+    const EXCEL_CELL_F1                 =   'Descripcion';
+    const EXCEL_CELL_G1                 =   'Numero de Paginas';
+    const EXCEL_CELL_H1                 =   'Imagen';
+    const EXCEL_DOWNLOAD_LOCATION       =   'http://books.linio/upload/';
+
+}
