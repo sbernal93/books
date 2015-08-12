3b6d05d261b1168ddfced8c6204ee3afd93d553b
Santiago Bernal <santiagobernal93@gmail.com>
2015-08-12 09:17:20 -0430

    Fixed SRP issue in google api adapter

--- src/AppBundle/Adapter/GoogleBooksApiAdapter.php
+++ src/AppBundle/Adapter/GoogleBooksApiAdapter.php
@@ -56,7 +56,7 @@ class GoogleBooksApiAdapter implements AdapterInterface
             if (count($items) > 0) {
                 $volumeInfo = $items[0]->getVolumeInfo();
 
-                return $this->buildBookWithApiInfo($volumeInfo);
+                return Book::buildBookWithGoogleApiInfo($volumeInfo);
             } else {
                 throw new BookNotFoundException("Google Book Api can't find ISBN: " . $isbn);
             }
@@ -85,7 +85,7 @@ class GoogleBooksApiAdapter implements AdapterInterface
                     $items = $result->getItems();
                     if (count($items) > 0) {
                         $volumeInfo = $items[0]->getVolumeInfo();
-                        $book = $this->buildBookWithApiInfo($volumeInfo);
+                        $book = Book::buildBookWithGoogleApiInfo($volumeInfo);
                         $data[] = $book;
                     }
                     else
@@ -104,59 +104,6 @@ class GoogleBooksApiAdapter implements AdapterInterface
             return $data;
         }
 
-    }
-
-    /**
-     * Builds a Book entity from the data retrieved from the API
-     *
-     * @param $volumeInfo
-     *
-     * @return Book
-     */
-    public function buildBookWithApiInfo($volumeInfo)
-    {
-        $numIsbn13 = 0;
-        $numIsbn10 = 1;
-        if(strlen($volumeInfo[Constants::GOOGLE_BOOKS_LABEL_INDUSTRY][0][Constants::GOOGLE_BOOKS_LABEL_IDENTIFIER]) != 13) {
-            $numIsbn10 = 0;
-            $numIsbn13 = 1;
-        }
-
-        if (isset($volumeInfo[Constants::GOOGLE_BOOKS_LABEL_INDUSTRY][$numIsbn13])) {
-            $isbn13 = $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_INDUSTRY][$numIsbn13][Constants::GOOGLE_BOOKS_LABEL_IDENTIFIER];
-        }
-        else
-        {
-            $isbn13 = "N/A";
-        }
-        if (isset($volumeInfo[Constants::GOOGLE_BOOKS_LABEL_INDUSTRY][$numIsbn10])) {
-            $isbn10 = $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_INDUSTRY][$numIsbn10][Constants::GOOGLE_BOOKS_LABEL_IDENTIFIER];
-        }
-        else
-        {
-            $isbn10 = "N/A";
-        }
-
-        $author = (is_array($volumeInfo[Constants::GOOGLE_BOOKS_LABEL_AUTHORS])) ?
-            implode(', ', $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_AUTHORS]) :
-            $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_AUTHORS];
-
-
-        $imageLink = (!empty($volumeInfo[Constants::GOOGLE_BOOKS_LABEL_IMAGELINKS][Constants::GOOGLE_BOOKS_LABEL_THUMBNAIL]))
-            ? $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_IMAGELINKS][Constants::GOOGLE_BOOKS_LABEL_THUMBNAIL] : '';
-
-
-        $book = Book::buildComplete(
-            $isbn10,
-            $isbn13,
-            $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_TITLE],
-            $author,
-            $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_PUBLISHER],
-            $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_DESCRIPTION],
-            $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_PAGECOUNT],
-            $imageLink
-        );
-        return $book;
     }
 
 }
--- src/AppBundle/Entity/Book.php
+++ src/AppBundle/Entity/Book.php
@@ -322,4 +322,57 @@ class Book
             $this->imageLink = $imageLink;
         }
     }
+
+    /**
+     * Builds a Book entity from the data retrieved from the API
+     *
+     * @param $volumeInfo
+     *
+     * @return Book
+     */
+    public static function buildBookWithGoogleApiInfo($volumeInfo)
+    {
+        $numIsbn13 = 0;
+        $numIsbn10 = 1;
+        if(strlen($volumeInfo[Constants::GOOGLE_BOOKS_LABEL_INDUSTRY][0][Constants::GOOGLE_BOOKS_LABEL_IDENTIFIER]) != 13) {
+            $numIsbn10 = 0;
+            $numIsbn13 = 1;
+        }
+
+        if (isset($volumeInfo[Constants::GOOGLE_BOOKS_LABEL_INDUSTRY][$numIsbn13])) {
+            $isbn13 = $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_INDUSTRY][$numIsbn13][Constants::GOOGLE_BOOKS_LABEL_IDENTIFIER];
+        }
+        else
+        {
+            $isbn13 = "N/A";
+        }
+        if (isset($volumeInfo[Constants::GOOGLE_BOOKS_LABEL_INDUSTRY][$numIsbn10])) {
+            $isbn10 = $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_INDUSTRY][$numIsbn10][Constants::GOOGLE_BOOKS_LABEL_IDENTIFIER];
+        }
+        else
+        {
+            $isbn10 = "N/A";
+        }
+
+        $author = (is_array($volumeInfo[Constants::GOOGLE_BOOKS_LABEL_AUTHORS])) ?
+            implode(', ', $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_AUTHORS]) :
+            $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_AUTHORS];
+
+
+        $imageLink = (!empty($volumeInfo[Constants::GOOGLE_BOOKS_LABEL_IMAGELINKS][Constants::GOOGLE_BOOKS_LABEL_THUMBNAIL]))
+            ? $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_IMAGELINKS][Constants::GOOGLE_BOOKS_LABEL_THUMBNAIL] : '';
+
+
+        $book = Book::buildComplete(
+            $isbn10,
+            $isbn13,
+            $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_TITLE],
+            $author,
+            $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_PUBLISHER],
+            $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_DESCRIPTION],
+            $volumeInfo[Constants::GOOGLE_BOOKS_LABEL_PAGECOUNT],
+            $imageLink
+        );
+        return $book;
+    }
 }
